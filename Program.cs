3dc117c65ba44fcf2270fe52e224ed711d583bd9
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2023-10-07 13:24:55 +0300

    Initial project

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+const string PR1 = "";
+const string PR2 = "";
+const string PR3 = "";
+const string PR4 = "";
